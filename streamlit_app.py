bd86819dbfc204193cc0066b4a586c8bb4661d58
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-02 15:06:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
+import os
 import streamlit as st
+from langchain.llms import OpenAI
 
-st.write('Hello World!')
+os.environ['OPENAI_API_KEY'] = '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'
+
+st.title('YoutTube GPT Creator')
+
+prompt = st.text_input('Plug in your prompt here')
+
+llm = OpenAI(temperature=0.9)
+
+if prompt:
+  response = llm(prompt)
+  st.write(response)
