514e92a7a0a8c149ed3194f2312c633d015d3dbd
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-02 17:06:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import os
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title('Ask A Question')
+st.title('LP AI Assistant')
 
-prompt = st.text_area('Write your prompt here')
+prompt = st.text_area('Write your prompt here:')
 
 llm = OpenAI(temperature=0.9)
 
