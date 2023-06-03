87febc723dffa98c5a6b27f52fa050718d406d96
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-03 19:36:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,15 +96,15 @@ else:
         
 # get csv for pandas agent
 if agent_type == 'Pandas Agent':
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-        bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
-        string_data = stringio.read()
-        st.write(string_data)
-        df = pd.read_csv(uploaded_file)    
+        uploaded_file = st.file_uploader("Choose a file")
+                if uploaded_file is not None:
+                        bytes_data = uploaded_file.getvalue()
+                        st.write(bytes_data)
+                        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+                        st.write(stringio)
+                        string_data = stringio.read()
+                        st.write(string_data)
+                        df = pd.read_csv(uploaded_file)    
         
         
 # prompt text
