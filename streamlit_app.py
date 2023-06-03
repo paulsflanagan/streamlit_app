b4ae3327e3c40085abb5ee718949b23819880d50
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-03 02:51:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,19 @@ from bs4 import BeautifulSoup
 
 
 
+# define background
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 # define ui
 st.title('LP AI Assistant')
 
