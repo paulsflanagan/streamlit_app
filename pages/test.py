5f37bf042406d98ba8f088895786fca58c76079b
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 14:43:55 +0100

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -4,8 +4,7 @@ import streamlit as st
 import csv
 
 
-llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
-master_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<Analysis>'
+
 
 
 st.title('AI Transcript Assistant')
