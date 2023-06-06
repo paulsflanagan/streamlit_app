9226ad4146a837bf606370dcce98c5c526a1af30
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 18:21:07 +0100

    Update Bot_Extractor.py

--- pages/Bot_Extractor.py
+++ pages/Bot_Extractor.py
@@ -11,6 +11,6 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     string_data = stringio.read()
-    data = json.load(string_data)
+    data = json.loads(string_data)
 
 
