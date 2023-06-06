c653b258e1b6b2620c2d1f501afcea9ff80a592b
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 17:03:11 +0100

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -57,7 +57,7 @@ task = """
 if st.button('Go'):
 
     for x in range(df.shape[0]):
-      st.write("Executing: " + str(x + 1) + " of " + str(df.shape[0]-1) + " : " + str(round(((x)/df.shape[0])*100)) +"% Complete ")
+      st.write("Executing: " + str(x + 1) + " of " + str(df.shape[0]) + " : " + str(round(((x)/df.shape[0])*100)) +"% Complete ")
       transcript = "Conversation ID: " + df['Conversation ID'][x] + "\n" + df['Transcript'][x]
       template = """
       % INSTRUCTIONS
