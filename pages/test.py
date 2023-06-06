69fbb7975f43966a4215a24b94ea9e5bcedc3f6c
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 14:39:00 +0100

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -3,7 +3,7 @@ from langchain import PromptTemplate
 import csv
 
 
-llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, model_name='gpt-3.5-turbo')
+llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
 master_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<Analysis>'
 
 
