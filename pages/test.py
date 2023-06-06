74d1cb01b665f1e228b6a3542a471498f04b88b3
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 14:58:47 +0100

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -3,3 +3,11 @@ import streamlit as st
 st.title('AI Transcript Assistant')
 
                                    
+from langchain.chat_models import ChatOpenAI
+from langchain import PromptTemplate
+llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, model_name='gpt-3.5-turbo')
+master_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<Analysis>'
+import csv
+
+
+
