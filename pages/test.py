eac4125d4d864251cb98bcd51e3f35fe062c7fbe
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 15:02:26 +0100

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -5,7 +5,7 @@ st.title('AI Transcript Assistant')
                                    
 from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate
-llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, model_name='gpt-3.5-turbo')
+llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
 master_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<Analysis>'
 import csv
 
