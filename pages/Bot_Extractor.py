8baf6a10a60c14b9979b0e99fba03db711a88184
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 18:15:05 +0100

    Update Bot_Extractor.py

--- pages/Bot_Extractor.py
+++ pages/Bot_Extractor.py
@@ -1,25 +1,23 @@
 import streamlit as st
-import csv
 import pandas as pd
-
-import json
-
 from io import StringIO
 
-
-
-
-st.title('LP Bot Extractor')
-
-uploaded_file = st.file_uploader("Upload a bot json file", accept_multiple_files=False)
-
+uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
+    # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-      #string_data = stringio.read()
-      #data = json.load(f)
-      #st.write(data)  
+    st.write(stringio)
 
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
 
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
 
 
