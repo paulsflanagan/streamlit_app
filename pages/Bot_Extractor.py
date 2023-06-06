930b371e471eb07bb39e869b16d73902f0120ff4
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 18:13:30 +0100

    Update Bot_Extractor.py

--- pages/Bot_Extractor.py
+++ pages/Bot_Extractor.py
@@ -14,9 +14,8 @@ st.title('LP Bot Extractor')
 uploaded_file = st.file_uploader("Upload a bot json file", accept_multiple_files=False)
 
 if uploaded_file is not None:
-    
-      bytes_data = uploaded_file.getvalue()
-      stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    bytes_data = uploaded_file.getvalue()
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
       #string_data = stringio.read()
       #data = json.load(f)
       #st.write(data)  
