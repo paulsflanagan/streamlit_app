f3491b6e057f431ca44686f1fdaf5cf7a78d828b
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 14:39:53 +0100

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -7,6 +7,8 @@ llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
 master_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<Analysis>'
 
 
+st.title('AI Transcript Assistant')
+
 # Import File
 
 file_name = ''
