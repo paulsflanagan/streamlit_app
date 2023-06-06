57f643c0aa19a0c992823307cab9f002e56929e4
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 15:35:24 +0100

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -16,7 +16,14 @@ master_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<Analysis>'
 
 uploaded_file = st.file_uploader("Upload a CSV file", accept_multiple_files=False)
 if uploaded_file is not None:
-    
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+    string_data = stringio.read()
+    st.write(string_data)
+    df = pd.read_csv(uploaded_file)
+    st.write("Active CSV: " + string_data)    
 
        #print("First Conversation ID: " + raw_data[0][0])
        #print("Last Conversation ID: " + raw_data[len(raw_data)-1][0])
