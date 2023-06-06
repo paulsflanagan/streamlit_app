5dd058a736f0da52901ca4841b7720ceb5299edb
Paul S Flanagan <paulsflanagan@gmail.com>
2023-06-06 15:34:20 +0100

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -16,11 +16,7 @@ master_xml = '<?xml version="1.0" encoding="UTF-8"?>\n<Analysis>'
 
 uploaded_file = st.file_uploader("Upload a CSV file", accept_multiple_files=False)
 if uploaded_file is not None:
-    st.write(uploaded_file.name)
-    test = uploaded_file.name
-    file = open(test, "r")
-    raw_data = list(csv.reader(file, delimiter=","))
-    file.close()
+    
 
        #print("First Conversation ID: " + raw_data[0][0])
        #print("Last Conversation ID: " + raw_data[len(raw_data)-1][0])
