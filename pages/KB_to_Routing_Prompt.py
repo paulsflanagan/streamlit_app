729ef5d86c7975070879e6f6981a365e469d5c8a
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-07 15:32:40 +0100

    Update KB_to_Routing_Prompt.py

--- pages/KB_to_Routing_Prompt.py
+++ pages/KB_to_Routing_Prompt.py
@@ -50,7 +50,7 @@ if uploaded_file is not None:
         bytes_data = uploaded_file.getvalue()
         #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
         #string_data = stringio.read()
-        df = pd.read_csv(uploaded_file, header=None)
+        df = pd.read_csv(uploaded_file)
 
         st.write(df)
         
