3457ffdae3ec437b0757b6fbd9840fd5f9f7469a
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-07 10:11:00 +0100

    Update Intents_to_Routing_Prompt.py

--- pages/Intents_to_Routing_Prompt.py
+++ pages/Intents_to_Routing_Prompt.py
@@ -21,12 +21,14 @@ if uploaded_file is not None:
     
     try:
         bytes_data = uploaded_file.getvalue()
-        #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        #string_data = stringio.read()
-        #df = pd.read_csv(uploaded_file)
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        string_data = stringio.read()
+        df = pd.read_csv(uploaded_file)
+
         
-        reader = csv.DictReader(bytes_data)
-        rows = list(reader)
+        
+        reader = csv.DictReader(df)
+        #rows = list(reader)
         intent_data = []
         armed = False
         #for i, row in enumerate(rows):
