612ea9298d34fd09c0002cb19721090d9e124bde
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-07 17:01:31 +0100

    Update KB_to_Routing_Prompt.py

--- pages/KB_to_Routing_Prompt.py
+++ pages/KB_to_Routing_Prompt.py
@@ -14,7 +14,7 @@ client = AzureOpenAI(
     azure_endpoint=st.secrets["azure_endpoint"]
 )
 
-sPromptIntentsFromKB = "Your job is to analyze the following article and provide a list of possible intents and description that could be served by that article. An intent is a short phrase (5 words or less) that describes something that a customer could want to do. A description is a short sentence that describes what the user is doing. You should provide at least one and not more than 10 possible intents and decription for the article. You should return each intent and description in this format: 1. Intent:<intent name>\nDescription:<description>\n2. Intent:..."
+sPromptIntentsFromKB = "Your job is to analyze the following article and provide a list of possible intents and description that could be served by that article. An intent is a short phrase (5 words or less) that describes something that a customer could want to do. A description is a short sentence that describes what the user is doing. You should provide at least one and not more than 10 possible intents and decription for the article. You should return each intent and description in this format: Intent:<intent name>\nDescription:<description>\n2. Intent:..."
 sPromptRoutesFromIntent = ""
 
 def call_oai(prompt, systemPrompt):
