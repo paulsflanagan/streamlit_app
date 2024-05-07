425236aced3494d5faa07b7acb781387842a0cd9
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-07 09:43:42 +0100

    Update Intents_to_Routing_Prompt.py

--- pages/Intents_to_Routing_Prompt.py
+++ pages/Intents_to_Routing_Prompt.py
@@ -21,6 +21,23 @@ if uploaded_file is not None:
     
     try:
         reader = csv.DictReader(uploaded_file.getvalue())
+        rows = list(reader)
+        intent_data = []
+        armed = False
+        for i, row in enumerate(rows):
+            # First row has intent names
+            if not intent_data:
+                for elem in row.keys():
+                    istruct = {'intent': elem, 'phrases': []}
+                    intent_data.append(istruct)
+            if len(row) == 0:
+                continue
+            if row[intent_data[0]['intent']] == "SampleSentences": 
+                armed = True
+                continue
+             if armed:
+                for intent_elem in intent_data:
+                    intent_elem['phrases'].append(row[intent_elem['intent']])
         #bytes_data = uploaded_file.getvalue()
         #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
         #string_data = stringio.read()
