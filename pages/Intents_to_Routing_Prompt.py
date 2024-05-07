9367fbd3e944d52218cbeeffa37a7c483d47cce3
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-07 09:35:59 +0100

    Update Intents_to_Routing_Prompt.py

--- pages/Intents_to_Routing_Prompt.py
+++ pages/Intents_to_Routing_Prompt.py
@@ -9,3 +9,21 @@ client = AzureOpenAI(
     api_version=st.secrets["api_version"],
     azure_endpoint=st.secrets["azure_endpoint"]
 )
+
+# Upload CSV
+
+uploaded_file = st.file_uploader("Upload an Intents CSV file", accept_multiple_files=False)
+if uploaded_file is not None:
+    
+    try:
+        bytes_data = uploaded_file.getvalue()
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        string_data = stringio.read()
+        df = pd.read_csv(uploaded_file)
+        df.columns = ['Conversation ID', 'Transcript']
+        st.write("First Conversation ID: " + df['Conversation ID'][0])
+        st.write("Last Conversation ID: " + df['Conversation ID'][df.shape[0]-1])
+        st.write("Conversation Count: " + str(df.shape[0]))
+    except UnicodeDecodeError:
+        st.write("Error Decoding CSV - Ensure encoding is utf-8")
+
