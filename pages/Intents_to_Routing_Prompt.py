beaaf92facaf2f1376ce4253f57dcb962f3fbfed
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-07 09:34:33 +0100

    Update Intents_to_Routing_Prompt.py

--- pages/Intents_to_Routing_Prompt.py
+++ pages/Intents_to_Routing_Prompt.py
@@ -1,6 +1,11 @@
 import json
 from openai import AzureOpenAI
-from google.colab import userdata
 import csv
 
 st.title('Intents To Routing Prompt')
+
+client = AzureOpenAI(
+    api_key=st.secrets["api_key"],
+    api_version=st.secrets["api_version"],
+    azure_endpoint=st.secrets["azure_endpoint"]
+)
