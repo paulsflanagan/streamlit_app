42d5b1be75712e2018a3d76bf66ad09d76bb06bf
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-07 17:19:50 +0100

    Update KB_to_Routing_Prompt.py

--- pages/KB_to_Routing_Prompt.py
+++ pages/KB_to_Routing_Prompt.py
@@ -17,9 +17,8 @@ client = AzureOpenAI(
 sPromptIntentsFromKB = """
 Your job is to analyze the following article and provide an intent and description that could be served by that article.
 An intent is a short phrase (5 words or less) that describes something that a customer could want to do. 
-A description is a sentence that describes what the user is doing. 
-You should provide at least one and not more than 10 possible intents and decription for the article. 
-You should return each intent and description in this format: Intent:<intent name>\nDescription:<description>\n2. Intent:..."""
+A description is a sentence that describes what the user is doing.
+You should return each intent and description in this format: Intent:<intent name>\nDescription:<description>"""
 sPromptRoutesFromIntent = ""
 
 def call_oai(prompt, systemPrompt):
