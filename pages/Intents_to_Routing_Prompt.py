2435f21d85d4924e58fab24522499629c08172f6
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-07 11:38:59 +0100

    Update Intents_to_Routing_Prompt.py

--- pages/Intents_to_Routing_Prompt.py
+++ pages/Intents_to_Routing_Prompt.py
@@ -59,18 +59,19 @@ if uploaded_file is not None:
         #rows = list(reader)
         intent_data = []
         armed = False
+        for z in range(df.shape[1]):
+            #for elem in row.keys():
+            istruct = {'intent': df[x].iloc[z], 'phrases': []}
+            intent_data.append(istruct)
+            st.write(intent_data)
+            
         for x in range(df.shape[1]):
             for y in range(df.shape[0]):
                 st.write(df[x].iloc[y])
                 #for i, row in enumerate(rows): // Ethans
                 # First row has intent names
-                if not intent_data:
-                    for z in range(df.shape[1]):
-                        #for elem in row.keys():
-                        istruct = {'intent': df[x].iloc[z], 'phrases': []}
-                        intent_data.append(istruct)
-                st.write(intent_data)
                 if len(row) == 0:
+                    #if len(row) == 0:
                     continue
                 if row[intent_data[0]['intent']] == "SampleSentences": 
                     armed = True
