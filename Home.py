c9d0af77efc3cdaea0ed98d4b538d45c3d950d1c
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-10 20:40:39 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,6 +12,9 @@ client = AzureOpenAI(
 )
 
 
+st.session_state.text = ""
+st.text_input("Your input here", key="text")
+
 spb_url = st.secrets["spb_url"]
 spb_key = st.secrets["spb_key"]
 
@@ -62,7 +65,7 @@ placeholder = st.empty()
 #time.sleep(3)
 
 systemPrompt = 'You are a helpful assistant.'
-userPrompt = st.text_input('Query', label_visibility = 'hidden', value = '')
+userPrompt = st.text_input('Query',key="text", label_visibility = 'hidden', value = '')
 
 # STRUCTURE HERE
 #response = supabase.table('StreamlitDB').select("*").execute()
