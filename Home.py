3f8c698459bb588e78c076062942fdc1f9ab46c5
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-10 20:13:53 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -59,7 +59,7 @@ def call_oai(userPrompt, systemPrompt):
 
 placeholder = st.empty()
 update_screen()
-userPrompt = st.text_input('Query')
+userPrompt = st.text_input('Query', label_visibility = 'hidden')
 # STRUCTURE HERE
 #response = supabase.table('StreamlitDB').select("*").execute()
 #testString = response.data[0]['test']
