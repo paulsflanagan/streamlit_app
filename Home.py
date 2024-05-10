912f08f916b46bf2e763ab25c6c7ed0b242f97c5
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-10 14:22:38 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,35 @@
+import streamlit as st
+from openai import AzureOpenAI
+
+st.title('Welcome')
+
+client = AzureOpenAI(
+    api_key=st.secrets["api_key"],
+    api_version=st.secrets["api_version"],
+    azure_endpoint=st.secrets["azure_endpoint"]
+)
+
+systemPrompt = ''
+userPrompt = ''
+
+def call_oai(prompt):
+    response = client.chat.completions.create(
+    model="llmgateway-text-35turbo-1106-model",
+    messages=[
+        {
+        "role": "system",
+        "content": systemPrompt
+        },
+        {
+        "role": "user",
+        "content": userPrompt
+        }
+    ],
+    temperature=0,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response.choices[0].message.content
 
