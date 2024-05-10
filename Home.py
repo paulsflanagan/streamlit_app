59d70405aa1268749466346d1ae7ec5919874e4e
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-10 18:47:10 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -28,9 +28,10 @@ supabase: Client = create_client(spb_url, spb_key)
 response = supabase.table('StreamlitDB').select("*").eq('user_name', 'paul.s.flanagan@gmail.com').execute()
 
 testString = ''
-testString = response.data[0]['user_query']
 
-#for x in response.data:
+for x in response.data:
+    tempTestString = response.data[x]['user_query']
+    testString = testString + tempTestString
     #testString = testString + 'User: ' + str(response.data[x]['user_query']) + '\nBot: ' + str(response.data[x]['llm_reponse'])
 
 #data, count = supabase.table('StreamlitDB').insert({"test": submit_string}).execute()
