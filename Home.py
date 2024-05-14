74d8fb696dc5057be80adefcbb9acfe2d8482e9d
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-14 12:53:30 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,11 +11,10 @@ st.title('Lorem ipsum dolor')
 placeholder = st.empty()
 
 text_to_display = '''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dapibus nunc, vulputate eleifend ex tincidunt sit amet. Mauris sit amet dolor nulla. Sed faucibus consectetur libero, varius consequat ipsum pellentesque vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris at interdum ex, vel egestas diam. Mauris sagittis mollis arcu et iaculis. Nulla a turpis finibus, tincidunt justo fermentum, luctus velit. Integer nisi lacus, aliquet vitae urna a, porta rutrum ex. Praesent sed aliquam sem. Nullam tincidunt ut sem dictum malesuada. Ut ut ligula nunc. Aenean consequat purus orci. Integer placerat pretium lorem, non fringilla mi tempus sit amet.
 
-Praesent id tristique erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed id libero elit. Morbi eu porta ipsum, eget tincidunt mauris. In cursus eget velit at sollicitudin. Morbi at purus neque. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+Football, also known as soccer in some countries, is a team sport played between two teams of eleven players each. The objective is to score goals by getting the ball into the opposing team's goal. Players primarily use their feet to kick the ball, but can also use their head or torso. The team with the most goals at the end of the game wins.
 
-In vitae risus sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In condimentum tempus sapien. Duis vel neque nec est pellentesque volutpat a quis urna. Integer sodales scelerisque sapien, non bibendum felis condimentum a. Proin quis nulla urna. Donec consectetur lorem enim, in sodales lorem maximus ac. Vestibulum et tortor sed justo tincidunt pretium. Pellentesque eget sapien varius, feugiat ante eu, viverra diam. In laoreet non tellus vitae pellentesque. Fusce volutpat neque neque, ut malesuada nisl blandit faucibus. Pellentesque faucibus maximus turpis eu rhoncus. Morbi blandit diam nec sem auctor molestie.
+Football is a popular sport played and watched by millions of people around the world. It requires skill, strategy, and teamwork, and is known for its fast pace and exciting matches. It is governed by the rules of the game set by the International Football Association Board (IFAB) and is played on a rectangular field with a goal at each end.
 '''
 
 split_text = text_to_display.split(" ")
