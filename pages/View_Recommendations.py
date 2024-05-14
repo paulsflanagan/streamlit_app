4d0a3cee64bcdbc597d3e24dd36e2ab694b44a99
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-14 15:45:26 +0100

    Update View_Recommendations.py

--- pages/View_Recommendations.py
+++ pages/View_Recommendations.py
@@ -10,5 +10,13 @@ supabase: Client = create_client(spb_url, spb_key)
 
 placeholder = st.empty()
 
-current_tickets = supabase.table('ticketsDB').select("*").eq('user_name', userName).execute()
+#current_tickets = supabase.table('ticketsDB').select("*").eq('user_name', userName).execute()
+current_tickets = supabase.table('ticketsDB').select("*").execute()
 
+with placeholder.container():
+  st.write("This is one element")
+  st.write("This is another")
+  count = 0
+  for x in response.data:
+      st.write('Created: ' + x['created_at'] + ' User: ' + x['creator'] + ' Reccomendation: ' + x['context'] + ' Completed: ' + str(x['context']))
+      st.write(' ')
