040cd692e92f61cfc38e575e0f230d3a904589f6
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-14 15:29:46 +0100

    Update Submit_Recommendation.py

--- pages/Submit_Recommendation.py
+++ pages/Submit_Recommendation.py
@@ -18,7 +18,7 @@ placeholder.write('Hello ' + st.experimental_user.email + '. Please submit your 
 recommendation = placeholder.text_area('Recommendation:', height=400, value='')
 
 
-if placeholder.button("Submit"):
+if st.button("Submit"):
   data, count = supabase.table('ticketsDB').insert({"creator": userName, "context": recommendation}).execute()
   placeholder.empty()
   placeholder = st.empty()
