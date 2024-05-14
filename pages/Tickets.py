c1010be7d012c2024e45dea327e9f13d012ec438
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-14 15:01:40 +0100

    Update Tickets.py

--- pages/Tickets.py
+++ pages/Tickets.py
@@ -10,6 +10,8 @@ supabase: Client = create_client(spb_url, spb_key)
 
 #current_tickets = supabase.table('ticketsDB').select("*").eq('user_name', userName).execute()
 
+userName = st.experimental_user.email
+st.write('Hello ' + st.experimental_user.email + '. Please ubmit your ticket below.')
 recommendation = st.text_area('Recommendation:', height=400, value='')
 
 
