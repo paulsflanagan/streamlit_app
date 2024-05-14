5e1675c08209de07de2afdd7a9ebefb59efd26f2
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-14 14:57:58 +0100

    Update Tickets.py

--- pages/Tickets.py
+++ pages/Tickets.py
@@ -8,7 +8,11 @@ spb_key = st.secrets["spb_key"]
 
 supabase: Client = create_client(spb_url, spb_key)
 
-current_tickets = supabase.table('ticketsDB').select("*").eq('user_name', userName).execute()
 
 
+st.text_area(
+if st.button("Submit"):
+  current_tickets = supabase.table('ticketsDB').select("*").eq('user_name', userName).execute()
+
+#data, count = supabase.table('ticketsDB').insert({"creator": userName, "user_query": userPrompt, "llm_response": llm_response}).execute()
 
