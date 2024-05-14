8b4f258ade0a7d0e2bd8a4cd9ef52049af81338c
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-14 14:45:16 +0100

    Update Tickets.py

--- pages/Tickets.py
+++ pages/Tickets.py
@@ -3,5 +3,12 @@ from supabase import create_client, Client
 
 st.title('Submit an Improvement Ticket')
 
+spb_url = st.secrets["spb_url"]
+spb_key = st.secrets["spb_key"]
+
+supabase: Client = create_client(spb_url, spb_key)
+
+
+
 
 
