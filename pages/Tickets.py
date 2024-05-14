4f2cabda405c66acd832eb88c7c804457f19957c
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-14 14:44:41 +0100

    Update Tickets.py

--- pages/Tickets.py
+++ pages/Tickets.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from openai import AzureOpenAI
 from supabase import create_client, Client
-import time
-import json
-import random
+
+st.title('Submit an Improvement Ticket')
 
 
-st.title('Lorem ipsum dolor')
+
