ec1832ddded3a7157819addecbdebc09dd8050a5
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 12:53:34 +0100

    Update Web_Scraper_Beta.py

--- pages/Web_Scraper_Beta.py
+++ pages/Web_Scraper_Beta.py
@@ -4,4 +4,15 @@ import requests
 URL = "https://en.wikipedia.org/wiki/A.I._Artificial_Intelligence"
 page = requests.get(URL)
 
-st.write(page.text)
+#st.write(page.text)
+
+text_buffer = ''
+for x in page.text
+  if x == '<':
+    reading = false
+  if x == '>':
+    reading = true
+  if reading
+    text_buffer = text_buffer + x
+  
+st.write(text_buffer)
