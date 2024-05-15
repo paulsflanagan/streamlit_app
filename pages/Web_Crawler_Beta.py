7f24e1223b2251f11c31415ab04a02ae93f248b8
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 15:06:21 +0100

    Update Web_Crawler_Beta.py

--- pages/Web_Crawler_Beta.py
+++ pages/Web_Crawler_Beta.py
@@ -10,23 +10,36 @@ url = st.text_input("Enter URL", "https://www.geeksforgeeks.org/")
 
 sep = '?'
 root = url
+completed_list = []
+buffer_list = []
+
+placeholder = st.empty()
 
 if st.button("Run"):
+
+    while len(buffer_list) > 0:
+        page = requests.get(url)
+            
+        soup = BeautifulSoup(page.content, "html.parser")
+            
+        results = soup.find()
+            
+        job_elements = results.find_all("a")
+            
     
-    page = requests.get(url)
+            
+        for job_element in job_elements:
+            #clean_job_element = RemoveHTMLTags(str(job_element))
+            if job_element.has_attr('href'):
+                if root in job_element['href']:
+                    if url not in buffer_list:
+                        buffer_list.append(job_element['href'].split(sep, 1)[0])
+                    
+        completed_list.append(url)
+        buffer_list.remove(url)
         
-    soup = BeautifulSoup(page.content, "html.parser")
-        
-    results = soup.find()
-        
-    job_elements = results.find_all("a")
-        
-    buffer_list = []
-        
-    for job_element in job_elements:
-        #clean_job_element = RemoveHTMLTags(str(job_element))
-        if job_element.has_attr('href'):
-            if root in job_element['href']:
-                buffer_list.append(job_element['href'].split(sep, 1)[0])
-        
-    st.write(str(buffer_list))
+        if len(buffer_list) > 0:
+            url = buffer_list[0]
+
+        placeholder.write("Completed: " + str(buffer_list))
+        placeholder.write("Buffer: " + str(buffer_list))
