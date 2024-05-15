313f56760d073a7493d67da75d36641d5e05ea67
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 12:50:46 +0100

    Update Web_Scraper_Beta.py

--- pages/Web_Scraper_Beta.py
+++ pages/Web_Scraper_Beta.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-URL = "https://paulsflanagan.com"
+URL = "https://en.wikipedia.org/wiki/A.I._Artificial_Intelligence"
 page = requests.get(URL)
 
 st.write(page.text)
