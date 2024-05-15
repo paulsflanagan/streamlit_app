47155004609c3f7159731f895c7f8e62fba8ee17
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 13:21:34 +0100

    Update Web_Scraper_Beta.py

--- pages/Web_Scraper_Beta.py
+++ pages/Web_Scraper_Beta.py
@@ -12,10 +12,13 @@ results = soup.find()
 job_elements = results.find_all("div")
 
 for job_element in job_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    st.write("Element: " + title_element.text + company_element.text + location_element.text)
+    title_element = job_element.find("h2")
+    company_element = job_element.find("h3")
+    location_element = job_element.find("p")
+    try:
+        st.write("Element: " + title_element.text + company_element.text + location_element.text)
+    except:
+        st.write("An element missing?")
 
 #st.write(page.text)
 #text_buffer = ''
