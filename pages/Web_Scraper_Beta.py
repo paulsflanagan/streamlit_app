3a82f4f522aefa0955e9b4bf59e6343d6da95b5f
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 13:15:12 +0100

    Update Web_Scraper_Beta.py

--- pages/Web_Scraper_Beta.py
+++ pages/Web_Scraper_Beta.py
@@ -1,23 +1,31 @@
 import streamlit as st
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://en.wikipedia.org/wiki/A.I._Artificial_Intelligence"
 page = requests.get(URL)
 
-#st.write(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
 
-text_buffer = ''
-previous_char = ''
-reading = True
-x = 0
-for current_char in page.text:
-  if current_char == '<' or current_char == '{' or current_char == '[':
-    reading = False
-  if previous_char == '>' or previous_char == '}' or previous_char == ']':
-    reading = True
-  if reading:
-    text_buffer = text_buffer + current_char
-  previous_char = current_char
-  x += 1
+results = soup.find()
+
+st.write(results)
+
+
+
+#st.write(page.text)
+#text_buffer = ''
+#previous_char = ''
+#reading = True
+#x = 0
+#for current_char in page.text:
+#  if current_char == '<' or current_char == '{' or current_char == '[':
+#    reading = False
+#  if previous_char == '>' or previous_char == '}' or previous_char == ']':
+#    reading = True
+#  if reading:
+#    text_buffer = text_buffer + current_char
+#  previous_char = current_char
+#  x += 1
   
-st.write(text_buffer)
+#st.write(text_buffer)
