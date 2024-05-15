d5004262e3d1156b3969683dd00a3ea543c3bc3b
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 09:17:01 +0100

    Update PDF_Scraper.py

--- pages/PDF_Scraper.py
+++ pages/PDF_Scraper.py
@@ -1,18 +1,32 @@
 
-
-
-# importing required modules 
 from pypdf import PdfReader 
+
+
+uploaded_file = st.file_uploader("", accept_multiple_files=False)
+if uploaded_file is not None:
+  try:
+    reader = PdfReader(uploaded_file)
+    st.write(len(reader.pages))
+      #bytes_data = uploaded_file.getvalue()
+      #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+      #string_data = stringio.read()
+      #additionalContext = string_data
+  except UnicodeDecodeError:
+    st.write("Error reading pdf")
+# importing required modules 
+
+
+
   
 # creating a pdf reader object 
-reader = PdfReader('example.pdf') 
+#reader = PdfReader('example.pdf') 
   
 # printing number of pages in pdf file 
-print(len(reader.pages)) 
+#print(len(reader.pages)) 
   
 # getting a specific page from the pdf file 
-page = reader.pages[0] 
+#page = reader.pages[0] 
   
 # extracting text from page 
-text = page.extract_text() 
-print(text) 
+#text = page.extract_text() 
+#print(text) 
