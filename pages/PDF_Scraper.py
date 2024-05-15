5d4453db4dea64f951b87e07e086fe72188a2cad
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 10:17:20 +0100

    Update PDF_Scraper.py

--- pages/PDF_Scraper.py
+++ pages/PDF_Scraper.py
@@ -12,7 +12,11 @@ if uploaded_file is not None:
     for each in reader.pages:
       text = text + "%PAGE: " + str(counter) + "\n\n" + each.extract_text() + "\n\n"
       counter += 1
+    strip_file_name = uploaded_file.name[:-4]
+    export_file_name = "Exported PDF Scrape - " + strip_file_name + ".txt"
+    st.download_button('Download Output', data=prompt_file, file_name=export_file_name)
     st.write(text)
+    
   except UnicodeDecodeError:
     st.write("Error reading pdf")
 
