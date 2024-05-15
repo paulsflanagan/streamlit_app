16a4b20f2e7a42940bffe3b134b1eaaff0b563ad
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 14:05:54 +0100

    Update Web_Scraper_Beta.py

--- pages/Web_Scraper_Beta.py
+++ pages/Web_Scraper_Beta.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import re
 
 def RemoveHTMLTags(strr):
@@ -10,16 +10,18 @@ def RemoveHTMLTags(strr):
 URL = "https://en.wikipedia.org/wiki/A.I._Artificial_Intelligence"
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
+#soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find()
+#results = soup.find()
 
-job_elements = results.find_all("p")
+#job_elements = results.find_all("p")
 
-buffer_string = ''
+#buffer_string = ''
 
-for job_element in job_elements:
-    clean_job_element = RemoveHTMLTags(str(job_element))
-    buffer_string = buffer_string + "\n\n" + clean_job_element
+#for job_element in job_elements:
+#    clean_job_element = RemoveHTMLTags(str(job_element))
+#    buffer_string = buffer_string + "\n\n" + clean_job_element
+
+buffer_string = RemoveHTMLTags(page)
 
 st.write(buffer_string)
