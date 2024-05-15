7d70cb870d2bcd537c0d0ce2213278a9f4f90fb9
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 14:28:27 +0100

    Update Web_Scraper_Beta.py

--- pages/Web_Scraper_Beta.py
+++ pages/Web_Scraper_Beta.py
@@ -10,18 +10,21 @@ def RemoveHTMLTags(strr):
 url = st.text_input("Enter URL", "")
 if st.button("Run"):
     #URL = "https://en.wikipedia.org/wiki/A.I._Artificial_Intelligence"
-    page = requests.get(url)
-    
-    soup = BeautifulSoup(page.content, "html.parser")
-    
-    results = soup.find()
-    
-    job_elements = results.find_all("p")
-    
-    buffer_string = ''
-    
-    for job_element in job_elements:
-        clean_job_element = RemoveHTMLTags(str(job_element))
-        buffer_string = buffer_string + "\n\n" + clean_job_element
-    
-    st.write(buffer_string)
+    try:
+        page = requests.get(url)
+        
+        soup = BeautifulSoup(page.content, "html.parser")
+        
+        results = soup.find()
+        
+        job_elements = results.find_all("p")
+        
+        buffer_string = ''
+        
+        for job_element in job_elements:
+            clean_job_element = RemoveHTMLTags(str(job_element))
+            buffer_string = buffer_string + "\n\n" + clean_job_element
+        
+        st.write(buffer_string)
+    except MissingSchema:
+        st.write(MissingSchema)
