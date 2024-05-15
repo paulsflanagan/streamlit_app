675bc24121b9a4722f440515b492ec076e656365
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 14:25:13 +0100

    Update Web_Scraper_Beta.py

--- pages/Web_Scraper_Beta.py
+++ pages/Web_Scraper_Beta.py
@@ -6,19 +6,22 @@ import re
 def RemoveHTMLTags(strr):
     return re.compile(r'<[^>]+>').sub('', strr)
 
-URL = "https://en.wikipedia.org/wiki/A.I._Artificial_Intelligence"
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
-
-results = soup.find()
-
-job_elements = results.find_all("p")
-
-buffer_string = ''
-
-for job_element in job_elements:
-    clean_job_element = RemoveHTMLTags(str(job_element))
-    buffer_string = buffer_string + "\n\n" + clean_job_element
-
-st.write(buffer_string)
+url = st.text_input("Enter URL", "")
+if st.button("Run"):
+    #URL = "https://en.wikipedia.org/wiki/A.I._Artificial_Intelligence"
+    page = requests.get(url)
+    
+    soup = BeautifulSoup(page.content, "html.parser")
+    
+    results = soup.find()
+    
+    job_elements = results.find_all("p")
+    
+    buffer_string = ''
+    
+    for job_element in job_elements:
+        clean_job_element = RemoveHTMLTags(str(job_element))
+        buffer_string = buffer_string + "\n\n" + clean_job_element
+    
+    st.write(buffer_string)
