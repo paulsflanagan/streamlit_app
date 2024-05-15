8fcf01463f2217ff320ba7f2a85f7e8122088b69
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-15 14:36:54 +0100

    Create Web_Crawler_Beta.py

---
+++ pages/Web_Crawler_Beta.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+
+
+url = st.text_input("Enter URL", "")
+if st.button("Run"):
+    #URL = "https://en.wikipedia.org/wiki/A.I._Artificial_Intelligence"
+    try:
+        page = requests.get(url)
+        
+        soup = BeautifulSoup(page.content, "html.parser")
+        
+        results = soup.find()
+        
+        job_elements = results.find_all("a")
+        
+        buffer_string = ''
+        
+        for job_element in job_elements:
+            #clean_job_element = RemoveHTMLTags(str(job_element))
+            buffer_string = buffer_string + "\n\n" + job_element
+        
+        st.write(buffer_string)
+    except MissingSchema:
+        st.write("Full url required: https://")
