4d1bc4f0407f02585e7244d73975cb6246901208
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-17 19:54:58 +0100

    Update Stock_Trading_Game_Account.py

--- pages/Stock_Trading_Game_Account.py
+++ pages/Stock_Trading_Game_Account.py
@@ -4,6 +4,10 @@ import json
 
 td_key=st.secrets["td_key"]
 
-
 st.title("Stock Trading Game - Account")
 
+userName = st.experimental_user.email
+spb_url = st.secrets["spb_url"]
+spb_key = st.secrets["spb_key"]
+
+supabase: Client = create_client(spb_url, spb_key)
