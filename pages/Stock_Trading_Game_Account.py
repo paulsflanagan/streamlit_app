32733e7fd2f28fdc71e4233979922de3250193dc
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-17 20:24:39 +0100

    Update Stock_Trading_Game_Account.py

--- pages/Stock_Trading_Game_Account.py
+++ pages/Stock_Trading_Game_Account.py
@@ -15,17 +15,18 @@ availableCash = 0
 stockOwned = []
 
 supabase: Client = create_client(spb_url, spb_key)
-data, push_count = supabase.table('StockTradingGame_AccountsDB').insert({"user_name": userName, "available_cash": 20000, "stock_owned": "[]"}).execute()
-account_details, pull_count = supabase.table('StockTradingGame_AccountsDB').select("*").eq('user_name', userName).execute()
-st.write(account_details)
-if pull_count == 0:
-  data, push_count = supabase.table('StockTradingGame_AccountsDB').insert({"user_name": userName, "available_cash": 20000, "stock_owned": "[]"}).execute()
-  availableCash = 0
-  stockOwned = []
-else:
-  availableCash = account_details['data']['available_cash']
-  stockOwned = account_details['data']['stock_owned']
+
+data, count = supabase.table('StockTradingGame_AccountsDB').insert({"user_name": userName, "available_cash": 20000, "stock_owned": "[]"}).execute()
+#account_details, pull_count = supabase.table('StockTradingGame_AccountsDB').select("*").eq('user_name', userName).execute()
+#st.write(account_details)
+#if pull_count == 0:
+#  data, push_count = supabase.table('StockTradingGame_AccountsDB').insert({"user_name": userName, "available_cash": 20000, "stock_owned": "[]"}).execute()
+#  availableCash = 0
+#  stockOwned = []
+#else:
+#  availableCash = account_details['data']['available_cash']
+#  stockOwned = account_details['data']['stock_owned']
   
-st.write(account_details)
-st.write(availableCash)
-st.write(stockOwned)
+#st.write(account_details)
+#st.write(availableCash)
+#st.write(stockOwned)
