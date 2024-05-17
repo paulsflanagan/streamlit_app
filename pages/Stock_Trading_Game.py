3c41051ce02544c4c293704ec3da25dff1543f29
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-17 18:08:38 +0100

    Update Stock_Trading_Game.py

--- pages/Stock_Trading_Game.py
+++ pages/Stock_Trading_Game.py
@@ -16,17 +16,16 @@ symbol = st.text_input("Enter a stock symbol 👇")
 if st.button("Check"):
   
   url = "https://api.twelvedata.com/time_series?apikey="+ td_key +"&interval=1min&format=JSON&symbol=" + symbol
-  try:
-    response = requests.get(url)
-  
-    st.write(response.status_code)
-    #st.write(response.json())
-    st.write("Symbol: " + response.json()["meta"][0]["symbol"])
-    st.write("Exchange: " + response.json()["meta"][0]["exchange"]) 
-    st.write("Type: " + response.json()["meta"][0]["type"]) 
-    st.write("Value (USD): " + response.json()["values"][0]["close"])
-  except:
-    st.write("Unable to find stock symbol")
+  #try:
+  response = requests.get(url)
+  st.write(response.status_code)
+  #st.write(response.json())
+  st.write("Symbol: " + response.json()["meta"][0]["symbol"])
+  st.write("Exchange: " + response.json()["meta"][0]["exchange"]) 
+  st.write("Type: " + response.json()["meta"][0]["type"]) 
+  st.write("Value (USD): " + response.json()["values"][0]["close"])
+  #except:
+   # st.write("Unable to find stock symbol")
 
 # Assuming the JSON string is stored in a variable called 'json_data'
 #json_data = '{"Global Quote": {"01. symbol": "IBM", "02. open": "168.2600", "03. high": "169.6300", "04. low": "167.7900", "05. price": "168.9700", "06. volume": "3492267", "07. latest trading day": "2024-05-16", "08. previous close": "168.2600", "09. change": "0.7100", "10. change percent": "0.4220%"}}'
