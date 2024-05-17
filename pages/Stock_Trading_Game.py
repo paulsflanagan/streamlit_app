1fdca7daa296c448bf5eb756e47dfa51a1bbe5f9
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-17 14:48:02 +0100

    Update Stock_Trading_Game.py

--- pages/Stock_Trading_Game.py
+++ pages/Stock_Trading_Game.py
@@ -1,6 +1,6 @@
 import requests
 import streamlit as st
-
+import json
 
 # Quote Api
 quoteApiUrl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo"
@@ -18,3 +18,17 @@ response = requests.get(quoteApiUrl)
 st.write(response.status_code)
 st.write(str(response.json()))
 st.write("Price: " + response.json()['Global Quote'])
+
+price = data_dict['Global Quote']['05. price']
+st.write(price)
+
+
+# Assuming the JSON string is stored in a variable called 'json_data'
+#json_data = '{"Global Quote": {"01. symbol": "IBM", "02. open": "168.2600", "03. high": "169.6300", "04. low": "167.7900", "05. price": "168.9700", "06. volume": "3492267", "07. latest trading day": "2024-05-16", "08. previous close": "168.2600", "09. change": "0.7100", "10. change percent": "0.4220%"}}'
+
+# Convert the JSON string to a Python dictionary
+#data_dict = json.loads(json_data)
+
+# Access the price
+
+  # Output: 168.
