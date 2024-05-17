6dee3a002b6c668837d448e94b31912191123cf6
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-17 17:59:43 +0100

    Update Stock_Trading_Game.py

--- pages/Stock_Trading_Game.py
+++ pages/Stock_Trading_Game.py
@@ -19,7 +19,7 @@ if st.button("Check"):
   response = requests.get(url)
   st.write(response.status_code)
   st.write(response.json())
-  st.write(response.json()['value'])
+  st.write(response.json().values[0].close)
 
 # Assuming the JSON string is stored in a variable called 'json_data'
 #json_data = '{"Global Quote": {"01. symbol": "IBM", "02. open": "168.2600", "03. high": "169.6300", "04. low": "167.7900", "05. price": "168.9700", "06. volume": "3492267", "07. latest trading day": "2024-05-16", "08. previous close": "168.2600", "09. change": "0.7100", "10. change percent": "0.4220%"}}'
