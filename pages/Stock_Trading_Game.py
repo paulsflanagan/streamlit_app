5e08f7e9deaf58500d097e2c9c176a6a21f1b8a8
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-17 14:20:25 +0100

    Update Stock_Trading_Game.py

--- pages/Stock_Trading_Game.py
+++ pages/Stock_Trading_Game.py
@@ -4,9 +4,8 @@ import streamlit as st
 
 url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
 
-page = requests.get(url)
+response = requests.get(url) 
 
-response = requests.get("https://api.open-notify.org/this-api-doesnt-exist") 
-print(response.status_code)
-
+st.write(response.status_code))
+st.write(response.json())
 #st.write(page)
