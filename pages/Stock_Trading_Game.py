0618a2e70aa756d8f89da79adfe653b9bad65efa
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-17 14:37:12 +0100

    Update Stock_Trading_Game.py

--- pages/Stock_Trading_Game.py
+++ pages/Stock_Trading_Game.py
@@ -2,10 +2,19 @@ import requests
 import streamlit as st
 
 
-url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+# Quote Api
+quoteApiUrl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo"
 
-response = requests.get(url) 
+#API DOCS
+#https://www.alphavantage.co/documentation/
+# Time Series Monthly
+#https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=IBM&apikey=demo
+#https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=TSCO.LON&apikey=demo
+
+
+response = requests.get(quoteApiUrl) 
 
 st.write(response.status_code)
 st.write(response.json())
-#st.write(page)
+
+
