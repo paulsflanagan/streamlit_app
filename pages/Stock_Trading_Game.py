303302023eeef0552a120db67591befdbc013748
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-18 02:03:11 +0100

    Update Stock_Trading_Game.py

--- pages/Stock_Trading_Game.py
+++ pages/Stock_Trading_Game.py
@@ -57,7 +57,7 @@ if symbol:
     value_list.append(row['close'])
     #take the date / time for x
     #and Close price for Y
-  df = pd.DataFrame(list(zip(datetime_list, value_list)), columns =['Date-Time', 'Value']).sort_values(by='Value', ascending=False)
+  df = pd.DataFrame(list(zip(datetime_list, value_list)), columns =['Date-Time', 'Value']).sort_values(by='Value', ascending=True)
   
   #st.write(df)
 
