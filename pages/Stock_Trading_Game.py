698a210550bb7bd7522d2338fb50bb0331f82d6c
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-18 02:31:05 +0100

    Update Stock_Trading_Game.py

--- pages/Stock_Trading_Game.py
+++ pages/Stock_Trading_Game.py
@@ -93,7 +93,8 @@ if symbol:
   amount = st.text_input("Enter an amount to purchase 👇")
   if amount:
     total_cost = float(amount) * float(currentValue) + (float(amount) * float(currentValue))*0.02
-    st.write("Cost of purchase: $" + str(round(total_cost,2)) + " - Including Fee: " + str(float(amount) * float(currentValue)*0.02)
+    fee = (float(amount) * float(currentValue))*0.02
+    st.write("Cost of purchase: $" + str(round(total_cost,2)) + " - Including Fee: " + fee)
     if float(total_cost) <= float(availableCash):
       if st.button("Buy Now"):
         if owns_current_stock:
