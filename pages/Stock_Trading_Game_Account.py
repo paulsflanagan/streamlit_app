7b62fa01dcda484509576ac90f0c44027116664c
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-18 03:35:11 +0100

    Update Stock_Trading_Game_Account.py

--- pages/Stock_Trading_Game_Account.py
+++ pages/Stock_Trading_Game_Account.py
@@ -21,7 +21,7 @@ supabase: Client = create_client(spb_url, spb_key)
 account_details, pull_count = supabase.table('StockTradingGame_AccountsDB').select("*").eq('user_name', userName).execute()
 st.write(account_details[1])
 
-if pull_count == None:
+if account_details[1] == []:
   st.write("Welcome to Stock Trading Game " + userName)
   data, push_count = supabase.table('StockTradingGame_AccountsDB').insert({"user_name": userName, "available_cash": 20000}).execute()
   availableCash = 0
