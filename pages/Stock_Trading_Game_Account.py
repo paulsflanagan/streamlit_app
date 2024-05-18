efa1e456c078915117f75359d72b8ceb2075c126
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-18 04:01:04 +0100

    Update Stock_Trading_Game_Account.py

--- pages/Stock_Trading_Game_Account.py
+++ pages/Stock_Trading_Game_Account.py
@@ -24,16 +24,21 @@ if account_details[1] == []:
   st.write("Welcome to Stock Trading Game")
   data, push_count = supabase.table('StockTradingGame_AccountsDB').insert({"user_name": userName, "available_cash": 20000}).execute()
   availableCash = 20000
+
+  st.subheader("Portfolio:")
+  st.write("Available Cash: $" + str(availableCash))
+
 else:
   availableCash = account_details[1][0]['available_cash']
   trade_details = supabase.table('StockTradingGame_OwnedStocksDB').select("*").eq('user_name', userName).execute()
-
+  
+  st.subheader("Portfolio:")
+  st.write("Available Cash: $" + str(availableCash))
   for row in trade_details.data:
       st.write("Stock: " + str(row['stock_symbol']) + " - Owned: " + str(row['stock_amount']) + " - Cost: " + str(row['stock_cost']))
 
 
-st.subheader("Portfolio:")
-st.write("Available Cash: $" + str(availableCash))
+
 
   #extracted_stocks_list = []
   #current_stock = ""
