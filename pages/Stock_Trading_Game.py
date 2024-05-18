522b502db74f2632e7d1a6e273b91fa5e2bf787d
Paul S Flanagan <paulsflanagan@gmail.com>
2024-05-18 03:20:51 +0100

    Update Stock_Trading_Game.py

--- pages/Stock_Trading_Game.py
+++ pages/Stock_Trading_Game.py
@@ -90,6 +90,8 @@ if symbol:
 
   if owns_current_stock:
     available_cash_display.write("Available Cash: $" + str(availableCash) + " - Currently Owned: " + str(amount_owned_current_stock) + " - Current Profit/Loss: " + str(round(current_stock_difference,2)))
+  else:
+    available_cash_display.write("Available Cash: $" + str(availableCash))
     
   amount = st.text_input("Enter an amount to purchase 👇")
   if amount:
