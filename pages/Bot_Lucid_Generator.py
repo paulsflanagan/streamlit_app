b89125e4766ea588db37e529b458fd37413b9714
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 21:49:34 +0100

    Update Bot_Lucid_Generator.py

--- pages/Bot_Lucid_Generator.py
+++ pages/Bot_Lucid_Generator.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from io import StringIO
+import requests
+import json
 
 st.title('Lucid Generator')
 
@@ -15,4 +17,20 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     if st.button("Submit"):
-        st.write("Boo")
+        url = 'https://api.lucid.co/oauth2/token'
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "code": code,
+            "client_id": l_cid,
+            "client_secret": l_cs,
+            "grant_type": "authorization_code",
+            "redirect_uri": l_ruri
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        tokenHolder = json.loads(response.text)
+        oAuth = tokenHolder['access_token']
+        st.write(oAuth)
+
