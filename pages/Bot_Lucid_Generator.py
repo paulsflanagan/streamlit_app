09bddf6757c90dd8d78b5b266119a32e22060170
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 22:01:59 +0100

    Update Bot_Lucid_Generator.py

--- pages/Bot_Lucid_Generator.py
+++ pages/Bot_Lucid_Generator.py
@@ -17,6 +17,8 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     if st.button("Submit"):
+        
+        # Create oAuth token
         url = 'https://api.lucid.co/oauth2/token'
         headers = {
             'Content-Type': 'application/json'
@@ -34,3 +36,28 @@ if uploaded_file is not None:
         oAuth = tokenHolder['access_token']
         st.write(oAuth)
 
+
+
+
+        
+
+
+
+        
+        # Create Chart
+        url = 'https://api.lucid.co/documents'
+        headers = {
+            'Authorization': 'Bearer '+ oAuth,
+            'Lucid-Api-Version': '1'
+        }
+        files = {
+            'file': ('import.lucid', open('import.lucid', 'rb'), 'x-application/vnd.lucid.standardImport')
+        }
+        data = {
+            'title': '_____Ais Test Extraction',
+            'product': 'lucidchart',
+            #'parent': '1234'
+        }
+        response = requests.post(url, headers=headers, files=files, data=data)
+        responseDict = json.loads(response.text)
+        st.write("Here is your Lucid: " + responseDict['editUrl'])
