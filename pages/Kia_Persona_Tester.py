cb993b1a8c324618d13978f9bfee0acb960fc244
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 23:36:23 +0100

    Update Kia_Persona_Tester.py

--- pages/Kia_Persona_Tester.py
+++ pages/Kia_Persona_Tester.py
@@ -9,6 +9,12 @@ client = AzureOpenAI(
     azure_endpoint=st.secrets["azure_endpoint"]
 )
 
+userName = st.experimental_user.email
+spb_url = st.secrets["spb_url"]
+spb_key = st.secrets["spb_key"]
+
+supabase: Client = create_client(spb_url, spb_key)
+
 st.title('Kia Persona Tester')
 
 
