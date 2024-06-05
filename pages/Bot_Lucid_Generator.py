ac788cb7da98a5e4388953a722f77e56cbf2dbc8
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 21:42:42 +0100

    Update Bot_Lucid_Generator.py

--- pages/Bot_Lucid_Generator.py
+++ pages/Bot_Lucid_Generator.py
@@ -2,14 +2,12 @@ import streamlit as st
 
 st.title('Lucid Generator')
 
-code = st.text_input("Enter Code", "")
-
 
 l_cid = st.secrets["l_cid"]
 l_cs = st.secrets["l_cs"]
 l_ruri = st.secrets["l_ruri"]
 
-st.write(l_ruri)
+code = st.text_input("Enter Code", "")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
