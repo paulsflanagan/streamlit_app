7b55188ed718ecc66a367b8e9fcc43d34532f30e
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 23:23:31 +0100

    Update Kia_Persona_Tester.py

--- pages/Kia_Persona_Tester.py
+++ pages/Kia_Persona_Tester.py
@@ -11,3 +11,34 @@ client = AzureOpenAI(
 
 st.title('ChatGPT Emulator V2b')
 
+
+def call_oai(userPrompt, systemPrompt):
+    
+    fullPrompt = [
+        {
+            "role": "system",
+            "content": systemPrompt
+        }
+    ]
+    
+    
+    fullPrompt.append(
+        {
+            "role": "user",
+            "content": userPrompt
+        }
+    )
+
+    #st.write(fullPrompt)
+    
+    response = client.chat.completions.create(
+    model="llmgateway-text-35turbo-1106-model",
+    messages=fullPrompt,
+    temperature=0,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response.choices[0].message.content, fullPrompt
+
