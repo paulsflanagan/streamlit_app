506e48d57d44edb94e646d11d4c416f02e4e92e4
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-06 00:50:44 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,6 +25,7 @@ st.write("- Stock Trading Game Shop: please note, you can buy but you cannot sel
 st.write("- Submit Recommendation: Please submit any issues or requests for tools")
 st.write("- Web Scraper Beta: Soon to be archived as this has moved to an asynch batch system. Unstable on browser.")
 
+st.write("All apps are open source: https://github.com/paulsflanagan/streamlit_app/")
 
 #@st.cache_data
 #def show_data():
