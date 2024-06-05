abc5fde817954d7504ffa1eedc2b5db62e6e287f
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 23:21:31 +0100

    Update Kia_Persona_Tester.py

--- pages/Kia_Persona_Tester.py
+++ pages/Kia_Persona_Tester.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from openai import AzureOpenAI
+
+## AZURE CLIENT
+
+client = AzureOpenAI(
+    api_key=st.secrets["api_key"],
+    api_version=st.secrets["api_version"],
+    azure_endpoint=st.secrets["azure_endpoint"]
+)
+
 
