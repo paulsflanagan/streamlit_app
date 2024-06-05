8592481aae68c161be715a559a7e4fb09a872218
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 21:40:05 +0100

    Update Bot_Lucid_Generator.py

--- pages/Bot_Lucid_Generator.py
+++ pages/Bot_Lucid_Generator.py
@@ -8,7 +8,7 @@ code = st.text_input("Enter Code", "")
 l_cid = st.secrets["l_cid"]
 l_cs = st.secrets["l_cs"]
 
-st.write(l_cid)
+
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
