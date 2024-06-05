fffa812095a7bb60c9dad1d251fe919479fb690d
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 23:29:13 +0100

    Update Kia_Persona_Tester.py

--- pages/Kia_Persona_Tester.py
+++ pages/Kia_Persona_Tester.py
@@ -9,7 +9,7 @@ client = AzureOpenAI(
     azure_endpoint=st.secrets["azure_endpoint"]
 )
 
-st.title('ChatGPT Emulator V2b')
+st.title('Kia Persona Tester')
 
 
 def call_oai(userPrompt, systemPrompt):
@@ -43,19 +43,21 @@ def call_oai(userPrompt, systemPrompt):
     return response.choices[0].message.content, fullPrompt
     
 systemPrompt = st.text_area('Write your system prompt here:', height=200, value="""
-    - You are an AI Bot that is very good at analysing conversation transcripts
-    - Your goal is to find relevant information from the transcript
-    - Only use information in the transcript provided
-    - For every opening tag you must add a closing tag
-    - Only use the tags provided. Do not create new tags
-    - Output in xml""")
+Your task is to rewrite the user message using the following persona:
+You are an empathetic KIA rep with automotive expertise, dedicated to providing exceptional service. You have deep knowledge of KIA vehicles and policies and are skilled in clear communication.
+
+Your Tone of Voice is defined as follows:
+1. Optimistic: Start every response with a positive affirmation or flourish and maintain an enthusiastic and proactive tone throughout the conversation.
+2. Fluid: Ensure the conversation flows smoothly, using natural, dynamic language that keeps the user engaged.
+3. Purposeful: Focus on delivering relevant, comprehensive, and goal-oriented responses that efficiently address the user's needs.
+4. Proficient: Demonstrate expertise and trustworthiness by providing clear, accurate, and concise information.
+
+Answer in a way that is positive and inspiring, understand the value of Kia’s product, have confidence in what you say and how you say it, talk about the experience of Kia and reflect inspiration through motion, movement, and fluidity. Re-write and return the user message. Make sure if there are multiple options in the response from the knowledgebase to return all of those options but rewrite the rest of the content to match the persona. When listing options, add a new line to separate these. Do not respond to the user.
+""")
+
 userPrompt = st.text_area('Write your user prompt here:', height=200, value="""
-    - You are an AI Bot that is very good at analysing conversation transcripts
-    - Your goal is to find relevant information from the transcript
-    - Only use information in the transcript provided
-    - For every opening tag you must add a closing tag
-    - Only use the tags provided. Do not create new tags
-    - Output in xml""")
+The EV6 is an all-new electric FLYING CAR!!!
+""")
 
 if st.button('Run'):
     call_oai(userPrompt, systemPrompt)
