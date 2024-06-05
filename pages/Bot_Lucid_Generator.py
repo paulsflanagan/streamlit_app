0676989308d67da668ed5c1ca6bb9db6020d550a
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 21:36:55 +0100

    Update Bot_Lucid_Generator.py

--- pages/Bot_Lucid_Generator.py
+++ pages/Bot_Lucid_Generator.py
@@ -4,6 +4,11 @@ st.title('Lucid Generator')
 
 code = st.text_input("Enter Code", "")
 
+
+api_key=st.secrets["api_key"],
+api_version=st.secrets["api_version"],
+azure_endpoint=st.secrets["azure_endpoint"]
+
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
