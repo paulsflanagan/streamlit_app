612d6597a1257465e63687a632625af25b7a5a45
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-05 21:29:23 +0100

    Update Bot_Lucid_Generator.py

--- pages/Bot_Lucid_Generator.py
+++ pages/Bot_Lucid_Generator.py
@@ -1,1 +1,8 @@
 import streamlit as st
+
+st.title('Lucid Generator')
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
