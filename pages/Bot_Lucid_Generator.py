66339f4c833bc47d25e838798174cdb33546a04a
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-06 11:42:52 +0100

    Update Bot_Lucid_Generator.py

--- pages/Bot_Lucid_Generator.py
+++ pages/Bot_Lucid_Generator.py
@@ -9,12 +9,20 @@ import os
 
 st.title('Lucid Generator')
 
+# Manually Input Lucid Creds
 
-l_cid = st.secrets["l_cid"]
-l_cs = st.secrets["l_cs"]
-l_ruri = st.secrets["l_ruri"]
+l_cid = st.text_input("Enter Lucid Client ID", "")
+l_cs = st.text_input("Enter Lucid Client Secret", "")
 
-st.write(' Visit: https://lucid.app/oauth2/authorize?client_id=P9FbSFWxRxWyQyP_qXaeZI6t1f2OFu9FJhRb1mbi&redirect_uri=https://lucid.app/oauth2/clients/P9FbSFWxRxWyQyP_qXaeZI6t1f2OFu9FJhRb1mbi/redirect&scope=lucidchart.document.content%20offline_access%20user.profile')
+if l_cid:
+    st.write(" Visit: https://lucid.app/oauth2/authorize?client_id=" + l_cid + "&redirect_uri=https://lucid.app/oauth2/clients/" + l_cid + "/redirect&scope=lucidchart.document.content%20offline_access%20user.profile")
+    l_ruri = "https://lucid.app/oauth2/clients/" + l_cid + "/redirect"
+
+#l_cid = st.secrets["l_cid"]
+#l_cs = st.secrets["l_cs"]
+#l_ruri = st.secrets["l_ruri"]
+
+#st.write(' Visit: https://lucid.app/oauth2/authorize?client_id='P9FbSFWxRxWyQyP_qXaeZI6t1f2OFu9FJhRb1mbi&redirect_uri=https://lucid.app/oauth2/clients/P9FbSFWxRxWyQyP_qXaeZI6t1f2OFu9FJhRb1mbi/redirect&scope=lucidchart.document.content%20offline_access%20user.profile')
 
 code = st.text_input("Enter Verification Code", "")
 
