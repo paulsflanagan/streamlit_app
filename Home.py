e6c354ed7b8addf9fbda30e160387bf64c62fb5a
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-06 01:13:17 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,27 +9,33 @@ st.write("")
 
 st.subheader("- AI Transcripts:")
 st.write("Locked down to Admins only. This will be archived as this has been moved to AI Studio.")
+st.write("https://share.vidyard.com/watch/vXm4akmZuRoBG46a1r8ENi?")
 
 st.subheader("- Bot Extractor:")
 st.write("Latest Update allows for extracting: Dialog Starter patterns and intents; Button options for multiple choice questions, quick replies, button type and structured engagements.")
+st.write("https://share.vidyard.com/watch/tj9CiLmJSEFeHbtQV8oJLN?")
 
 st.subheader("- Bot Lucid Generator:")
 st.write("Locked to Admin only through verification code. Soon to be released to GA.")
 
 st.subheader("- Chat Gpt Emulator V2 Beta:")
 st.write("Open for Beta Testing. Latest Updates: allowing for querying files (text based and pdf) and historical context.")
+st.write("https://share.vidyard.com/watch/SaYKSoAi8Fts1DH63USQg8?")
 
 st.subheader("- Intents to Routing Prompts:")
 st.write("No Changes.")
+st.write("https://share.vidyard.com/watch/t1B6ECZL1XzXJMd6Jvh67P?")
 
 st.subheader("- KB to Routing Prompts:")
 st.write("No Changes.")
+st.write("https://share.vidyard.com/watch/sX3ZH2NbY8kPH7eaHhwfAw?")
 
 st.subheader("- Kia Persona Tester:")
 st.write("Temporary Script added to allow Kia to test Personification through our Azure llm.")
 
 st.subheader("- Optimise KB for AI Search:")
 st.write("No Changes.")
+st.write("https://share.vidyard.com/watch/5eNuLb5wAbvSj3Kdi1ownp?")
 
 st.subheader("- PDF Scraper:")
 st.write("No Changes.")
