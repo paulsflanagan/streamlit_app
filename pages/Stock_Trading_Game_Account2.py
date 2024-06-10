80cc1114352fe5cbd49e37ab5ed809d8cc84e769
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-10 22:22:59 +0100

    Update Stock_Trading_Game_Account2.py

--- pages/Stock_Trading_Game_Account2.py
+++ pages/Stock_Trading_Game_Account2.py
@@ -20,26 +20,12 @@ st.title("Stock Trading Game - Account2")
 st.write(userName)
 
 
-availableCash = 0
-
-
-
-
-account_details, pull_count = supabase.table('StockTradingGame_AccountsDB').select("*").eq('user_name', userName).execute()
-
-if account_details[1] == []:
-  st.write("Welcome to Stock Trading Game")
-  data, push_count = supabase.table('StockTradingGame_AccountsDB').insert({"user_name": userName, "available_cash": 20000}).execute()
-  availableCash = 20000
-
-  st.subheader("Portfolio:")
-  st.write("Available Cash: $" + str(availableCash))
-
-else:
-  availableCash = account_details[1][0]['available_cash']
-  trade_details = supabase.table('StockTradingGame_OwnedStocksDB').select("*").eq('user_name', userName).execute()
+availableCash = getAvailableCash(userName)
   
-  st.subheader("Portfolio:")
-  st.write("Available Cash: $" + str(availableCash))
-  for row in trade_details.data:
-      st.write("Stock: " + str(row['stock_symbol']) + " - Owned: " + str(row['stock_amount']) + " - Cost: " + str(row['stock_cost']))
+st.subheader("Portfolio:")
+st.write("Available Cash: $" + str(availableCash))
+
+
+
+for row in trade_details.data:
+    st.write("Stock: " + str(row['stock_symbol']) + " - Owned: " + str(row['stock_amount']) + " - Cost: " + str(row['stock_cost']))
