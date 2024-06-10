b2c5304265095b464c0f809650db5155f8273eb6
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-10 22:20:36 +0100

    Update Stock_Trading_Game_Account2.py

--- pages/Stock_Trading_Game_Account2.py
+++ pages/Stock_Trading_Game_Account2.py
@@ -6,7 +6,6 @@ import json
 #### Keys
 td_key=st.secrets["td_key"]
 userName = st.experimental_user.email
-st.write(userName)
 
 spb_url = st.secrets["spb_url"]
 spb_key = st.secrets["spb_key"]
@@ -18,8 +17,9 @@ supabase: Client = create_client(spb_url, spb_key)
 #### UI
 
 st.title("Stock Trading Game - Account2")
+st.write(userName)
+
 
 availableCash = 0
 
 
-account_details, pull_count = supabase.table('StockTradingGame_AccountsDB').select("*").eq('user_name', userName).execute()
