87b9c39952dba0371a807a155e4dfd971cc988d4
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-10 22:24:32 +0100

    Update Stock_Trading_Game_Account2.py

--- pages/Stock_Trading_Game_Account2.py
+++ pages/Stock_Trading_Game_Account2.py
@@ -14,6 +14,28 @@ spb_key = st.secrets["spb_key"]
 supabase: Client = create_client(spb_url, spb_key)
 
 
+#### Functions
+
+### DataBase Functions
+
+## Initialize Account
+def initialiseAccount(userName):
+  account_details, push_count = supabase.table('StockTradingGame_AccountsDB').insert({"user_name": userName, "available_cash": 20000}).execute()
+  return account_details
+
+## Account Details
+def getAccountDetails(userName):
+  account_details, pull_count = supabase.table('StockTradingGame_AccountsDB').select("*").eq('user_name', userName).execute()
+  if account_details[1] == []:
+    initialiseAccount(userName)
+    account_details, pull_count = supabase.table('StockTradingGame_AccountsDB').select("*").eq('user_name', userName).execute()
+  return account_details[1][0]
+
+def getAvailableCash(userName):
+  account_details = getAccountDetails(userName)
+  return account_details['available_cash']
+
+
 #### UI
 
 st.title("Stock Trading Game - Account2")
