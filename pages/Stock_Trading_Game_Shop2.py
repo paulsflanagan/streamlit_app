b30714d3cebbf7ecf33e9ce86b0312439116cdb7
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-10 16:19:38 +0100

    Update Stock_Trading_Game_Shop2.py

--- pages/Stock_Trading_Game_Shop2.py
+++ pages/Stock_Trading_Game_Shop2.py
@@ -193,30 +193,26 @@ symbol = st.text_input("Enter a stock symbol 👇")
 if symbol:
 
   stockData = getStockData(symbol)
-
-  st.write(stockData)
   
   chartData = stockData["stock_data"]["values"]
-  
-  st.write(chartData)
-  #st.write(response)
 
   #df = pd.read_json(chart_data)
   #df = pd.read_json(str(chart_data))
   #st.write(df)
 
   ### FOR EACH IN VALUES
-#  datetime_list = []
-#  value_list = []
-#  for row in chart_data:
+
+  datetime_list = []
+  value_list = []
+  for row in chart_data:
     #st.write("DateTime): " + row['datetime'])
-#    datetime_list.append(row['datetime'])
+    datetime_list.append(row['datetime'])
     #st.write("Value (USD): " + row['close'])
-#    value_list.append(row['close'])
+    value_list.append(row['close'])
     #take the date / time for x
     #and Close price for Y
-#  df = pd.DataFrame(list(zip(datetime_list, value_list)), columns =['Date-Time', 'Value'])#.sort_values(by='Value', ascending=True)
-#  df['Value'] = df['Value'].astype(float)
+  df = pd.DataFrame(list(zip(datetime_list, value_list)), columns =['Date-Time', 'Value'])#.sort_values(by='Value', ascending=True)
+  df['Value'] = df['Value'].astype(float)
 
 
 
