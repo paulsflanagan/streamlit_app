9acbe64ace8502bb98fc5220dc10d60f27216f36
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-12 14:09:34 +0100

    Update Stock_Trading_Game_Statistics.py

--- pages/Stock_Trading_Game_Statistics.py
+++ pages/Stock_Trading_Game_Statistics.py
@@ -76,25 +76,10 @@ def getStockValue(symbol):
     return 'Invalid Stock Symbol'
 
 #### Showcase Functions
-def getTotalOwnedStocks():
-  trades_data = supabase.table('StockTradingGame_OwnedStocksDB').select("*").execute()
-  total_owned_data = {}
-  for each in trades_data.data:
-    stock_symbol = each['stock_symbol']
-    stock_amount = each['stock_amount']
-    if total_owned_data.get(stock_symbol) == None:
-      total_owned_data[stock_symbol] = stock_amount
-    else:
-      buffer_amount = total_owned_data[stock_symbol] + stock_amount
-      total_owned_data[stock_symbol] = buffer_amount
-  return total_owned_data
 
-def getOrderdTotalOwnedStocks():
-  total_owned_data = getTotalOwnedStocks()
-  c = Counter(total_owned_data)
-  ordered_total_owned_data = c.most_common()
-  return ordered_total_owned_data
-
+def getTotalOwnedStocksStatistic():
+  total_owned_stock, pull_count = supabase.table('StockTradingGame_StatisticsDB').select("total_owned_stock").execute()
+  print(total_owned_stock[1][0]['total_owned_stock'])
 
 def getHighestValuePlayer():
   account_details = supabase.table('StockTradingGame_AccountsDB').select("*").execute()
@@ -124,11 +109,9 @@ st.title("Stock Trading Game - Player Statistics")
 st.write(userName)
 
 st.subheader("Statistics:")
-st.write("Top Player Owned Stocks:")
-topOwnedStocks = getOrderdTotalOwnedStocks()
-for each in topOwnedStocks:
-  st.write("Stock: " + each[0] + " - Owned: " + str(each[1]))
-st.write("")  
+topOwnedStocks = getTotalOwnedStocksStatistic()
+st.write("Top Player Owned Stocks " + topOwnedStocks)
+
 st.write("Top Value Player:")
 topValuePlayer = getOrderdHighestValuePlayer()
 for each in topValuePlayer:
