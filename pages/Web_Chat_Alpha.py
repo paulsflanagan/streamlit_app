40ad765832ae810ad0432744c01fc884f3421aaf
Paul S Flanagan <paulsflanagan@gmail.com>
2024-06-30 00:58:02 +0100

    Update Web_Chat_Alpha.py

--- pages/Web_Chat_Alpha.py
+++ pages/Web_Chat_Alpha.py
@@ -1,1 +1,19 @@
+import streamlit as st
+from supabase import create_client, Client
+import time
+
+## SECRETS
+
+userName = st.experimental_user.email
+spb_url = st.secrets["spb_url"]
+spb_key = st.secrets["spb_key"]
+
+supabase: Client = create_client(spb_url, spb_key)
+
+
+
+## UI HERE
+
+st.title('Messenger - Alpha')
+
 
