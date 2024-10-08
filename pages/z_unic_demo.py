d8fb300b8e269f5b059119aceb093b4db83ebbf1
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-08 17:10:53 +0100

    Update z_unic_demo.py

--- pages/z_unic_demo.py
+++ pages/z_unic_demo.py
@@ -7,6 +7,28 @@ ais_auth_email = st.secrets["ais_auth_email"]
 ais_auth_pw = st.secrets["ais_auth_pw"]
 ais_flow_id = st.secrets["ais_flow_id"]
 
-st.title('UNIC Demo' + ais_flow_id )
+st.title('UNIC Demo')
 
+#Generate Auth Token
+import requests
 
+url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + ais_auth_geo
+headers = {
+    'Content-Type': 'application/json',
+}
+data = {
+    "returnSecureToken": True,
+    "email": ais_auth_email,
+    "password": ais_auth_pw
+
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+if response.status_code == 200:
+    st.write('Success:', response.json())
+else:
+    st.write('Error:', response.text)
+
+idToken = response.json()['idToken']
+st.write(idToken)
