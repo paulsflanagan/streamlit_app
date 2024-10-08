33af0c0ac9f2a199c91f401e3bf547844e4d27b9
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-08 17:05:42 +0100

    Update z_unic_demo.py

--- pages/z_unic_demo.py
+++ pages/z_unic_demo.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import requests
 
 
+ais_auth_geo = st.secrets["ais_auth_geo"]
+ais_auth_email = st.secrets["ais_auth_email"]
+ais_auth_pw = st.secrets["ais_auth_pw"]
+ais_flow_id = st.secrets["ais_flow_id "]
 
-st.title('UNIC Demo')
+st.title('UNIC Demo' + ais_flow_id )
 
 
