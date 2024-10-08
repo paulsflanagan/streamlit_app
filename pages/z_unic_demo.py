74956f89d6977369f53cdcffed0a2f7d90ca95c5
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-08 17:09:13 +0100

    Update z_unic_demo.py

--- pages/z_unic_demo.py
+++ pages/z_unic_demo.py
@@ -5,7 +5,7 @@ import requests
 ais_auth_geo = st.secrets["ais_auth_geo"]
 ais_auth_email = st.secrets["ais_auth_email"]
 ais_auth_pw = st.secrets["ais_auth_pw"]
-ais_flow_id = st.secrets["ais_flow_id "]
+ais_flow_id = st.secrets["ais_flow_id"]
 
 st.title('UNIC Demo' + ais_flow_id )
 
