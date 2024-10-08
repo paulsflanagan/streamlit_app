4c792017439994e6b86478e232cae5842b88d06b
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-08 17:12:11 +0100

    Update z_unic_demo.py

--- pages/z_unic_demo.py
+++ pages/z_unic_demo.py
@@ -11,24 +11,24 @@ st.title('UNIC Demo')
 
 #Generate Auth Token
 import requests
-
-url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + ais_auth_geo
-headers = {
-    'Content-Type': 'application/json',
-}
-data = {
-    "returnSecureToken": True,
-    "email": ais_auth_email,
-    "password": ais_auth_pw
-
-}
-
-response = requests.post(url, headers=headers, json=data)
-
-if response.status_code == 200:
-    st.write('Success:', response.json())
-else:
-    st.write('Error:', response.text)
-
-idToken = response.json()['idToken']
-st.write(idToken)
+if st.button("test"):
+    url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + ais_auth_geo
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    data = {
+        "returnSecureToken": True,
+        "email": ais_auth_email,
+        "password": ais_auth_pw
+    
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        st.write('Success:', response.json())
+    else:
+        st.write('Error:', response.text)
+    
+    idToken = response.json()['idToken']
+    st.write(idToken)
