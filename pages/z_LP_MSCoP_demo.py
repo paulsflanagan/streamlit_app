2a918cf2d1583528675e76fe34765a67968dcb33
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-18 16:42:36 +0100

    Update z_LP_MSCoP_demo.py

--- pages/z_LP_MSCoP_demo.py
+++ pages/z_LP_MSCoP_demo.py
@@ -21,9 +21,9 @@ else:
 ## AZURE CLIENT
 
 client = AzureOpenAI(
-    api_key=st.secrets["api_key"],
-    api_version=st.secrets["api_version"],
-    azure_endpoint=st.secrets["azure_endpoint"]
+    api_key=st.secrets["api_key_us"],
+    api_version=st.secrets["api_version_us"],
+    azure_endpoint=st.secrets["azure_endpoint_us"]
 )
 
 ## SECRETS
@@ -147,7 +147,7 @@ def call_oai(userPrompt, systemPrompt, conversation_history, additionalContext):
     #st.write(fullPrompt)
     
     response = client.chat.completions.create(
-    model="llmgateway-text-35turbo-1106-model",
+    model="gpt-4o-mini",
     messages=fullPrompt,
     temperature=0,
     max_tokens=256,
