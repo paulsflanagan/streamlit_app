aa6aad800c923756c6493feae1b657d21e3d1a79
Paul S Flanagan <paulsflanagan@gmail.com>
2024-10-19 01:55:40 +0100

    Update z_Quantas_Co-Pilot_Demo.py

--- pages/z_Quantas_Co-Pilot_Demo.py
+++ pages/z_Quantas_Co-Pilot_Demo.py
@@ -10,7 +10,7 @@ import os
 from bs4 import BeautifulSoup
 
 
-URLs = ["https://www.qantas.com/qcom5-sitemap.xml","https://www.qantas.com/qcom2-sitemap.xml"]
+URLs = ["https://www.qantas.com/qcom2-sitemap.xml"]
 #,"https://www.qantas.com/qcom4-sitemap.xml" - Airport Guides
 #,"https://www.qantas.com/qcom3-sitemap.xml" - onbord information /seat maps
 #,"https://www.qantas.com/qcom6-sitemap.xml" - frequent flyer / mebeships
